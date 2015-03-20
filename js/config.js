1647c81bf3fed02236b82e34ae0e61069ea3419a
Martin Spier <spiermar@gmail.com>
2015-03-20 10:49:40 -0700

    Changing default pmwebd port.

--- js/config.js
+++ js/config.js
@@ -23,7 +23,7 @@
 var config = angular.module('vector.config', []);
 
 config.constant('vectorConfig', {
-  'port': 7402,
+  'port': 44323,
   'interval': 2,
   'window': 2,
   'enableCpuFlameGraph': false,
