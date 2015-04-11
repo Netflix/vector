22f6bcebb1b8caec633c5b9b073122af2b7f22aa
dylanjohnson <therealdylanjohnson@gmail.com>
2015-04-11 17:40:04 -0400

    Make eslint pass for filters

--- app/js/filters.js
+++ app/js/filters.js
@@ -24,7 +24,7 @@
 
 /* Filters */
 
-var filters = angular.module('app.filters', []);
+angular.module('app.filters', []);
 
 var uniqueItems = function (data, key) {
     var result = [],
@@ -39,24 +39,30 @@ var uniqueItems = function (data, key) {
     return result;
 };
 
-filters.filter('groupBy', function () {
-    return function (collection, key) {
-        if (collection === null) {
-            return;
-        }
-        return uniqueItems(collection, key);
-    };
-});
-
-filters.filter('groupFilter', function () {
-    return function (collection, group) {
-        var result = [],
-            i;
-        for (i = 0; i < collection.length; i++) {
-            if (collection[i].group === group) {
-                result.push(collection[i]);
+angular
+    .module('app.filters')
+    .filter('groupBy', function () {
+        return function (collection, key) {
+            if (collection === null) {
+                return;
             }
-        }
-        return result;
-    };
-})
+            return uniqueItems(collection, key);
+        };
+    })
+;
+
+angular
+    .module('app.filters')
+    .filter('groupFilter', function () {
+        return function (collection, group) {
+            var result = [],
+                i;
+            for (i = 0; i < collection.length; i++) {
+                if (collection[i].group === group) {
+                    result.push(collection[i]);
+                }
+            }
+            return result;
+        };
+    })
+;
