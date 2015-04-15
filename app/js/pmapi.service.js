c790635db890ca65aab69542fc13c32700ee9e84
Martin Spier <spiermar@gmail.com>
2015-04-14 17:21:06 -0700

    Reducing context fetch timeout to 10s. No point on waiting 60s for context.

--- app/js/pmapi.service.js
+++ app/js/pmapi.service.js
@@ -29,6 +29,7 @@
             settings.params = {};
             settings.params[params.contextType] = params.contextValue;
             settings.params.polltimeout = params.pollTimeout.toString();
+            settings.timeout = 10000;
 
             return $http(settings)
                 .then(function (response) {
