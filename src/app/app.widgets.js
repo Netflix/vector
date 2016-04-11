555ed2d326d5e92d6487320bf6091ad1f84707d6
hc000 <hc000@users.noreply.github.com>
2016-04-11 12:33:41 -0700

    renamed container network widget due to duplication

--- src/app/app.widgets.js
+++ src/app/app.widgets.js
@@ -804,7 +804,7 @@
                     enableVerticalResize: false,
                     group: 'Container'
                 },{
-                    name: 'network.interface.bytes',
+                    name: 'container.network.interface.bytes',
                     title: 'Container Network Throughput (kB)',
                     directive: 'line-time-series',
                     dataAttrName: 'data',
