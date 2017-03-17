6f12ba351263bfaba9f51b2f31adf0ed152ee7c9
mohsini172 <mohsini172@gmail.com>
2017-03-18 00:26:15 +0500

    Removed the debug log

--- src/app/components/dashboard/dashboard.service.js
+++ src/app/components/dashboard/dashboard.service.js
@@ -311,9 +311,6 @@
         function getGuid() {
             return Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
         }
-        $rootScope.$on("angular-resizable.resizeEnd", function (event, args) {
-            console.log(args);
-        });
         return {
             cancelInterval: cancelInterval,
             updateInterval: updateInterval,
