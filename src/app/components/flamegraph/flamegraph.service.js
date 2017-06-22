eedeb39aefea8af9be7d651dc0af23e096cdba33
Brendan Gregg <bgregg@netflix.com>
2017-06-22 00:22:55 +0000

    retry flamegraph if an unread DONE is returned

--- src/app/components/flamegraph/flamegraph.service.js
+++ src/app/components/flamegraph/flamegraph.service.js
@@ -23,12 +23,14 @@
      */
      function FlameGraphService($log, $rootScope, $http, toastr) {
         var svgname = "";
+        var canretry = true;
 
         /**
         * @name generate
         * @desc
         */
         function generate() {
+            canretry = true;
             $http.get($rootScope.properties.protocol + '://' + $rootScope.properties.host + ':' + $rootScope.properties.port + '/pmapi/' + $rootScope.properties.context + '/_fetch?names=generic.cpuflamegraph')
                 .success(function (data) {
                     if (angular.isDefined(data.values[0])) {
@@ -37,6 +39,12 @@
                         if (fields[0] == "REQUESTED") {
                             svgname = fields[1];
                             toastr.success('generic.cpuflamegraph requested.', 'Success');
+                        } else if (fields[0] == "DONE") {
+                            // since this is a generate call, retry
+                            if (canretry) {
+                                generate();
+                                canretry = false;	// avoid infinite recursion
+                            }
                         } else {
                             toastr.success('generic.cpuflamegraph already in progress (' + message + '), please wait.', 'Success');
                         }
@@ -50,7 +58,7 @@
 
         function getSvgName() {
             return svgname;
-       }
+        }
 
         return {
             generate: generate,
