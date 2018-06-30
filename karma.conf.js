e03cb90821c07cb6827db485ab320f984ea81d08
Jason Koch <jkoch@netflix.com>
2018-06-29 17:05:36 -0700

    make basic karma tests work (fail though)

--- karma.conf.js
+++ karma.conf.js
@@ -1,7 +1,26 @@
 'use strict';
 
 var path = require('path');
-var conf = require('./gulp/conf');
+
+var conf = {
+  paths: { src: 'src' },
+  vendorJs: [
+    "node_modules/jquery/dist/jquery.js",
+    "node_modules/angular/angular.js",
+    "node_modules/angular-route/angular-route.js",
+    "node_modules/bootstrap/dist/js/bootstrap.js",
+    "node_modules/angular-ui-bootstrap/dist/ui-bootstrap-tpls.js",
+    "node_modules/lodash/lodash.js",
+    "node_modules/angular-toastr/dist/angular-toastr.tpls.js",
+    "node_modules/moment/moment.js",
+    "node_modules/jquery-ui-dist/jquery-ui.js",
+    "node_modules/angular-ui-sortable/dist/sortable.js",
+    "node_modules/malhar-angular-dashboard/dist/malhar-angular-dashboard.js",
+    "node_modules/d3/d3.js",
+    "node_modules/nvd3/build/nv.d3.js",
+    "node_modules/angular-sanitize/angular-sanitize.js"
+  ]
+}
 
 var _ = require('lodash');
 
