3f0b84372066d7e906482dbd992085200dc05de3
Jason Koch <jkoch@netflix.com>
2018-08-14 11:39:59 -0700

    avoid updating chart if datasets have not changed

--- src/app/components/Chart/Chart.jsx
+++ src/app/components/Chart/Chart.jsx
@@ -91,6 +91,13 @@ class Chart extends React.Component {
     modalOpen: false
   }
 
+  shouldComponentUpdate(nextProps /*, nextState */) {
+    // avoid a render call if the dataset has not changed, this avoids unnecessary polls when
+    // the context data changes but the dataset itself has not changed, to do this we store a copy
+    // in state
+    return (this.props.datasets !== nextProps.datasets)
+  }
+
   render () {
     const { chartInfo, datasets, onCloseClicked, onNewSettings, containerList, instanceDomainMappings, containerId, pmids } = this.props
 
